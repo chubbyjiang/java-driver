a5e1f58eadb068606591f2a08d1c9f48040ed2c2
Andrew Tolbert <andrew.tolbert@datastax.com>
2017-08-11 16:33:40 -0500

    Unify ExportAsStringTest into one class for all versions

--- driver-core/src/test/java/com/datastax/driver/core/ExportAsStringTest.java
+++ driver-core/src/test/java/com/datastax/driver/core/ExportAsStringTest.java
@@ -32,7 +32,7 @@ import org.testng.annotations.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
-@CassandraVersion("2.2")
+@CassandraVersion("2.0")
 @CCMConfig(config = "enable_user_defined_functions:true")
 public class ExportAsStringTest extends CCMTestsSupport {
 
@@ -51,7 +51,7 @@ public class ExportAsStringTest extends CCMTestsSupport {
      * is failed.
      */
     @Test(groups = "short")
-    public void create_schema_and_ensure_exported_cql_is_expected() {
+    public void create_schema_and_ensure_exported_cql_is_as_expected() {
         String keyspace = "complex_ks";
         Map<String, Object> replicationOptions = ImmutableMap.<String, Object>of("class", "SimpleStrategy", "replication_factor", 1);
 
@@ -61,77 +61,91 @@ public class ExportAsStringTest extends CCMTestsSupport {
         // create session from this keyspace.
         Session session = cluster().connect(keyspace);
 
-        // Usertype 'ztype' with two columns.  Given name to ensure that even though it has an alphabetically
-        // later name, it shows up before other user types ('ctype') that depend on it.
-        session.execute(SchemaBuilder.createType("ztype")
-                .addColumn("c", DataType.text()).addColumn("a", DataType.cint()));
+        KeyspaceMetadata ks;
 
-        // Usertype 'xtype' with two columns.  At same level as 'ztype' since both are depended on by ctype, should
-        // show up before 'ztype' because it's alphabetically before, even though it was created after.
-        session.execute(SchemaBuilder.createType("xtype")
-                .addColumn("d", DataType.text()));
+        // udts require 2.1+
+        if (ccm().getCassandraVersion().compareTo(VersionNumber.parse("2.1")) >= 0) {
+            // Usertype 'ztype' with two columns.  Given name to ensure that even though it has an alphabetically
+            // later name, it shows up before other user types ('ctype') that depend on it.
+            session.execute(SchemaBuilder.createType("ztype")
+                    .addColumn("c", DataType.text()).addColumn("a", DataType.cint()));
 
-        KeyspaceMetadata ks = cluster().getMetadata().getKeyspace(keyspace);
+            // Usertype 'xtype' with two columns.  At same level as 'ztype' since both are depended on by ctype, should
+            // show up before 'ztype' because it's alphabetically before, even though it was created after.
+            session.execute(SchemaBuilder.createType("xtype")
+                    .addColumn("d", DataType.text()));
 
-        // Usertype 'ctype' which depends on both ztype and xtype, therefore ztype and xtype should show up earlier.
-        session.execute(SchemaBuilder.createType("ctype")
-                .addColumn("z", ks.getUserType("ztype").copy(true))
-                .addColumn("x", ks.getUserType("xtype").copy(true)));
+            ks = cluster().getMetadata().getKeyspace(keyspace);
 
-        // Usertype 'btype' which has no dependencies, should show up before 'xtype' and 'ztype' since it's
-        // alphabetically before.
-        session.execute(SchemaBuilder.createType("btype").addColumn("a", DataType.text()));
+            // Usertype 'ctype' which depends on both ztype and xtype, therefore ztype and xtype should show up earlier.
+            session.execute(SchemaBuilder.createType("ctype")
+                    .addColumn("z", ks.getUserType("ztype").copy(true))
+                    .addColumn("x", ks.getUserType("xtype").copy(true)));
 
-        // Refetch keyspace for < 3.0 schema this is required as a new keyspace metadata reference may be created.
-        ks = cluster().getMetadata().getKeyspace(keyspace);
+            // Usertype 'btype' which has no dependencies, should show up before 'xtype' and 'ztype' since it's
+            // alphabetically before.
+            session.execute(SchemaBuilder.createType("btype").addColumn("a", DataType.text()));
 
-        // Usertype 'atype' which depends on 'ctype', so should show up after 'ctype', 'xtype' and 'ztype'.
-        session.execute(SchemaBuilder.createType("atype")
-                .addColumn("c", ks.getUserType("ctype").copy(true)));
+            // Refetch keyspace for < 3.0 schema this is required as a new keyspace metadata reference may be created.
+            ks = cluster().getMetadata().getKeyspace(keyspace);
 
-        // A simple table with a udt column and LCS compaction strategy.
-        session.execute(SchemaBuilder.createTable("ztable")
-                .addPartitionKey("zkey", DataType.text())
-                .addColumn("a", ks.getUserType("atype").copy(true))
-                .withOptions().compactionOptions(SchemaBuilder.leveledStrategy().ssTableSizeInMB(95)));
+            // Usertype 'atype' which depends on 'ctype', so should show up after 'ctype', 'xtype' and 'ztype'.
+            session.execute(SchemaBuilder.createType("atype")
+                    .addColumn("c", ks.getUserType("ctype").copy(true)));
 
-        // A table that will have materialized views (copied from mv docs)
-        session.execute(SchemaBuilder.createTable("cyclist_mv").addPartitionKey("cid", DataType.uuid())
-                .addColumn("name", DataType.text())
-                .addColumn("age", DataType.cint())
-                .addColumn("birthday", DataType.date())
-                .addColumn("country", DataType.text()));
+            // A simple table with a udt column and LCS compaction strategy.
+            session.execute(SchemaBuilder.createTable("ztable")
+                    .addPartitionKey("zkey", DataType.text())
+                    .addColumn("a", ks.getUserType("atype").copy(true))
+                    .withOptions().compactionOptions(SchemaBuilder.leveledStrategy().ssTableSizeInMB(95)));
+        } else {
+            // A simple table with LCS compaction strategy.
+            session.execute(SchemaBuilder.createTable("ztable")
+                    .addPartitionKey("zkey", DataType.text())
+                    .addColumn("a", DataType.cint())
+                    .withOptions().compactionOptions(SchemaBuilder.leveledStrategy().ssTableSizeInMB(95)));
+        }
 
-        // index on table with view, index should be printed first.
-        session.execute(SchemaBuilder.createIndex("cyclist_by_country")
-                .onTable("cyclist_mv")
-                .andColumn("country"));
+        // date type requries 2.2+
+        if (ccm().getCassandraVersion().compareTo(VersionNumber.parse("2.2")) >= 0) {
+            // A table that will have materialized views (copied from mv docs)
+            session.execute(SchemaBuilder.createTable("cyclist_mv").addPartitionKey("cid", DataType.uuid())
+                    .addColumn("name", DataType.text())
+                    .addColumn("age", DataType.cint())
+                    .addColumn("birthday", DataType.date())
+                    .addColumn("country", DataType.text()));
 
-        // materialized views require 3.0+
-        if (ccm().getCassandraVersion().compareTo(VersionNumber.parse("3.0")) >= 0) {
-            // A materialized view for cyclist_mv, reverse clustering.  created first to ensure creation order does not
-            // matter, alphabetical does.
-            session.execute("CREATE MATERIALIZED VIEW cyclist_by_r_age " +
-                    "AS SELECT age, birthday, name, country " +
-                    "FROM cyclist_mv " +
-                    "WHERE age IS NOT NULL AND cid IS NOT NULL " +
-                    "PRIMARY KEY (age, cid) " +
-                    "WITH CLUSTERING ORDER BY (cid DESC)"
-            );
+            // index on table with view, index should be printed first.
+            session.execute(SchemaBuilder.createIndex("cyclist_by_country")
+                    .onTable("cyclist_mv")
+                    .andColumn("country"));
 
-            // A materialized view for cyclist_mv, select *
-            session.execute("CREATE MATERIALIZED VIEW cyclist_by_a_age " +
-                    "AS SELECT * " +
-                    "FROM cyclist_mv " +
-                    "WHERE age IS NOT NULL AND cid IS NOT NULL " +
-                    "PRIMARY KEY (age, cid)");
+            // materialized views require 3.0+
+            if (ccm().getCassandraVersion().compareTo(VersionNumber.parse("3.0")) >= 0) {
+                // A materialized view for cyclist_mv, reverse clustering.  created first to ensure creation order does not
+                // matter, alphabetical does.
+                session.execute("CREATE MATERIALIZED VIEW cyclist_by_r_age " +
+                        "AS SELECT age, birthday, name, country " +
+                        "FROM cyclist_mv " +
+                        "WHERE age IS NOT NULL AND cid IS NOT NULL " +
+                        "PRIMARY KEY (age, cid) " +
+                        "WITH CLUSTERING ORDER BY (cid DESC)"
+                );
 
-            // A materialized view for cyclist_mv, select columns
-            session.execute("CREATE MATERIALIZED VIEW cyclist_by_age " +
-                    "AS SELECT age, birthday, name, country " +
-                    "FROM cyclist_mv " +
-                    "WHERE age IS NOT NULL AND cid IS NOT NULL " +
-                    "PRIMARY KEY (age, cid) WITH comment = 'simple view'");
+                // A materialized view for cyclist_mv, select *
+                session.execute("CREATE MATERIALIZED VIEW cyclist_by_a_age " +
+                        "AS SELECT * " +
+                        "FROM cyclist_mv " +
+                        "WHERE age IS NOT NULL AND cid IS NOT NULL " +
+                        "PRIMARY KEY (age, cid)");
+
+                // A materialized view for cyclist_mv, select columns
+                session.execute("CREATE MATERIALIZED VIEW cyclist_by_age " +
+                        "AS SELECT age, birthday, name, country " +
+                        "FROM cyclist_mv " +
+                        "WHERE age IS NOT NULL AND cid IS NOT NULL " +
+                        "PRIMARY KEY (age, cid) WITH comment = 'simple view'");
+            }
         }
 
         // A table with a secondary index, taken from documentation on secondary index.
@@ -149,26 +163,29 @@ public class ExportAsStringTest extends CCMTestsSupport {
                 .onTable("rank_by_year_and_name")
                 .andColumn("rank"));
 
-        // UDFs
-        session.execute("CREATE OR REPLACE FUNCTION avgState ( state tuple<int,bigint>, val int ) CALLED ON NULL INPUT RETURNS tuple<int,bigint> LANGUAGE java AS \n" +
-                "  'if (val !=null) { state.setInt(0, state.getInt(0)+1); state.setLong(1, state.getLong(1)+val.intValue()); } return state;';");
-        session.execute("CREATE OR REPLACE FUNCTION avgFinal ( state tuple<int,bigint> ) CALLED ON NULL INPUT RETURNS double LANGUAGE java AS \n" +
-                "  'double r = 0; if (state.getInt(0) == 0) return null; r = state.getLong(1); r /= state.getInt(0); return Double.valueOf(r);';");
+        // udfs and udas require 2.22+
+        if (ccm().getCassandraVersion().compareTo(VersionNumber.parse("2.2")) >= 0) {
+            // UDFs
+            session.execute("CREATE OR REPLACE FUNCTION avgState ( state tuple<int,bigint>, val int ) CALLED ON NULL INPUT RETURNS tuple<int,bigint> LANGUAGE java AS \n" +
+                    "  'if (val !=null) { state.setInt(0, state.getInt(0)+1); state.setLong(1, state.getLong(1)+val.intValue()); } return state;';");
+            session.execute("CREATE OR REPLACE FUNCTION avgFinal ( state tuple<int,bigint> ) CALLED ON NULL INPUT RETURNS double LANGUAGE java AS \n" +
+                    "  'double r = 0; if (state.getInt(0) == 0) return null; r = state.getLong(1); r /= state.getInt(0); return Double.valueOf(r);';");
 
-        // UDAs
-        session.execute("CREATE AGGREGATE IF NOT EXISTS mean ( int ) \n" +
-                "SFUNC avgState STYPE tuple<int,bigint> FINALFUNC avgFinal INITCOND (0,0);");
-        session.execute("CREATE AGGREGATE IF NOT EXISTS average ( int ) \n" +
-                "SFUNC avgState STYPE tuple<int,bigint> FINALFUNC avgFinal INITCOND (0,0);");
+            // UDAs
+            session.execute("CREATE AGGREGATE IF NOT EXISTS mean ( int ) \n" +
+                    "SFUNC avgState STYPE tuple<int,bigint> FINALFUNC avgFinal INITCOND (0,0);");
+            session.execute("CREATE AGGREGATE IF NOT EXISTS average ( int ) \n" +
+                    "SFUNC avgState STYPE tuple<int,bigint> FINALFUNC avgFinal INITCOND (0,0);");
+        }
 
         ks = cluster().getMetadata().getKeyspace(keyspace);
 
         // validate that the exported schema matches what was expected exactly.
-        assertThat(ks.exportAsString().trim()).isEqualTo(getExpectedCqlString(ccm().getCassandraVersion()));
+        assertThat(ks.exportAsString().trim()).isEqualTo(getExpectedCqlString());
     }
 
-    static String getExpectedCqlString(VersionNumber versionNumber) {
-        String majorMinor = versionNumber.getMajor() + "." + versionNumber.getMinor();
+    private String getExpectedCqlString() {
+        String majorMinor = ccm().getCassandraVersion().getMajor() + "." + ccm().getCassandraVersion().getMinor();
         String resourceName = "/export_as_string_test_" + majorMinor + ".cql";
 
         Closer closer = Closer.create();
